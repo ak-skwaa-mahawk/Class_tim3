046f7b48cf17e29664268af03ca7453a954a3392
John Carroll <jcarroll@twomilesolutions.com>
2026-09-19 21:28:14 -0800

    Update unicode_utils.h

--- unicode_utils.h
+++ unicode_utils.h
@@ -1,8 +1,9 @@
-/* unicode_utils.h */
+/* unicode_utils.h additions */
 #ifndef UNICODE_UTILS_H
 #define UNICODE_UTILS_H
 
 #include "unicode_constants.h"
+#include <stdio.h>
 
 typedef struct {
     const uint8_t *src;
@@ -15,5 +16,6 @@ bool utf8_next_codepoint(Utf8Iterator *iter, uint32_t *out_cp, size_t *out_bytes
 bool utf8_validate_string(const char *str, size_t max_bytes);
 int unicode_codepoint_width(uint32_t cp);
 size_t utf8_terminal_width(const char *str);
+void utf8_print_truncated_padded(const char *str, size_t target_width);
 
 #endif /* UNICODE_UTILS_H */
