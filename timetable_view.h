3c609d8d37f03ff2394b11cb30e838ebb958a220
John Carroll <jcarroll@twomilesolutions.com>
2026-09-19 21:25:50 -0800

    Create timetable_view.h

---
+++ timetable_view.h
@@ -0,0 +1,10 @@
+/* timetable_view.h */
+#ifndef TIMETABLE_VIEW_H
+#define TIMETABLE_VIEW_H
+
+#include "timetable.h"
+
+void timetable_print_day(const TimeTable *tt, WeekDay day);
+void timetable_print_compact_week(const TimeTable *tt);
+
+#endif /* TIMETABLE_VIEW_H */
